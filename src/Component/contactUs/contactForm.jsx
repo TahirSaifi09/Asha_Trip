bc3c288ee870173140df19eac1ac370f7fad1228
Tahir Saifi <tahirsaifi09>
2025-01-24 18:11:45 +0530

    responsive Contact page done!

--- src/Component/contactUs/contactForm.jsx
+++ src/Component/contactUs/contactForm.jsx
@@ -1,9 +1,9 @@
 export default function contactForm() {
   return (
     <div className=" h-full bg-slate-50 ">
-      <form className="flex justify-center py-16">
-        <div className="flex w-1/2 flex-wrap bg-white rounded-xl shadow-lg p-8">
-          <div className="w-1/2 h-12 pr-4 ">
+      <form className="flex justify-center py-16 max-md:py-4 max-md:px-4 max-lg:px-8">
+        <div className="flex w-1/2 max-lg:w-full max-md:px-4 max-md:py-8 flex-wrap bg-white rounded-xl shadow-lg p-8 xl:w-7/12 max-md:p-0">
+          <div className="w-1/2 max-md:w-full h-12 pr-4 max-md:pr-0 ">
             <input
               type="text"
               name="name"
@@ -12,7 +12,7 @@ export default function contactForm() {
               className="w-full h-full px-4 bg-slate-200 outline-none rounded placeholder-black"
             />
           </div>
-          <div className="w-1/2 h-12 pl-4">
+          <div className="w-1/2 max-md:w-full h-12 pl-4 max-md:pl-0 max-md:mt-4">
             <input
               type="email"
               name="email"
@@ -21,7 +21,7 @@ export default function contactForm() {
               className="w-full h-full px-4 bg-slate-200 outline-none rounded placeholder-black"
             />
           </div>
-          <div className="w-1/2 h-12 pr-4 mt-8">
+          <div className="w-1/2 max-md:w-full h-12 pr-4 max-md:pr-0 mt-8 max-md:mt-4">
             <input
               type="tel"
               name="tel"
@@ -30,7 +30,7 @@ export default function contactForm() {
               className="w-full h-full px-4 bg-slate-200 outline-none rounded placeholder-black"
             />
           </div>
-          <div className="w-1/2 h-12 pl-4 mt-8">
+          <div className="w-1/2 max-md:w-full max-md:pl-0 h-12 pl-4 mt-8 max-md:mt-4">
             <input
               type="text"
               name="subject"
@@ -39,21 +39,21 @@ export default function contactForm() {
               className="w-full h-full px-4 bg-slate-200 outline-none rounded placeholder-black"
             />
           </div>
-          <div>
+          <div className="w-full">
             <textarea
               name="textarea"
               placeholder="Message"
-              rows="4"
+              rows="5"
               cols="100"
               required
-              className="w-full mt-8 bg-slate-200 p-4 outline-none rounded placeholder-black "
+              className="w-full mt-4 bg-slate-200 p-4 outline-none rounded placeholder-black "
             />
           </div>
           <div className="flex justify-end w-full">
             <input
               type="submit"
               value="Send Message"
-              className="bg-blue-600 text-white px-6 py-3 rounded-xl mt-8 shadow-lg hover:text-black hover:bg-white duration-700 font-medium "
+              className="bg-blue-600 text-white px-6 py-3 rounded-xl mt-4 shadow-lg hover:text-black hover:bg-white duration-700 font-medium "
             />
             </div>
         </div>
--- src/Component/contactUs/firstPage.jsx
+++ src/Component/contactUs/firstPage.jsx
@@ -1,13 +1,13 @@
 export default function FirstPage() {
   return (
     <div>
-      <div className=" px-10 h-[300px] w-full bg-slate-50">
+      <div className="px-10 max-sm:px-2 xl:h-[300px] w-full max-lg:py-4 bg-slate-50">
         <div className="flex justify-between items-center bg-slate-50">
-          <img src="vacation.png" alt="vacation" className="h-72 p-8" />
-          <h1 className="text-center pl-8 text-5xl font-bold w-1/3">
+          <img src="vacation.png" alt="vacation" className="h-72 max-sm:h-20 max-md:h-32 max-lg:h-48 max-sm:mt-6 p-8 max-sm:p-1 max-md:p-3" />
+          <h1 className="text-center pl-8 max-lg:pl-0 text-5xl max-sm:text-2xl font-bold w-1/3">
             Contact Us
           </h1>
-          <img src="Location.png" alt="plan" className="w-1/4 bg-slate-200" />
+          <img src="Location.png" alt="plan" className="w-1/4 max-sm:w-1/4 max-sm: bg-slate-200" />
         </div>
       </div>
     </div>
