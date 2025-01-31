53aaff734312b0ea91bc0bb11856f7faf973fe9a
Tahir Saifi <tahirsaifi413@gmail.com>
2025-01-31 18:23:11 +0530

    Remove login and signup page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, } from "./Pages";
-import Login from "./app/Login/Login";
-import Signup from "./app/Login/Signup";
 import TripBooking from "./Pages/TripBooking/index";
 import TourPackage from "./Pages/TourPackage";
 import PrivacyPage from "./Pages/Page/PrivacyPage";
@@ -12,8 +10,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/TripBooking" element={<TripBooking />}/>
         <Route path="/TourPackage" element={<TourPackage/>}/>
         <Route path="/privacyPage" element={<PrivacyPage/>}/>
